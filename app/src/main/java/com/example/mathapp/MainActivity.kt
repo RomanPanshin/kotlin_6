e73086470d77ee34566db72b85e766bc902f3831
Roman Panshin <roma.super@icloud.com>
2024-05-26 20:52:53 +0300

    remade structure

--- app/src/main/java/com/example/mathapp/MainActivity.kt
+++ app/src/main/java/com/example/mathapp/MainActivity.kt
@@ -10,21 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.mathapp.ui.theme.MathAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MathAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
         }
     }
 }
@@ -40,7 +32,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MathAppTheme {
-        Greeting("Android")
-    }
+
 }
